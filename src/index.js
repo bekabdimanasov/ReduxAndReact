b538c13ea3e31de0ebcfe7e22d60d665282e7a21
bekabdimanasov <bekabdimanasov@gmail.com>
2020-12-09 10:35:01 +0300

    4.1 Подготовка данных для страницы товара

--- src/index.js
+++ src/index.js
@@ -2,28 +2,27 @@ import './main.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
-import { createBrowserHistory } from 'history'
+import {createStore, applyMiddleware} from 'redux'
+import {createBrowserHistory} from 'history'
 import thunk from 'redux-thunk'
-import { routerMiddleware, ConnectedRouter } from 'connected-react-router'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import { Provider } from 'react-redux'
+import {routerMiddleware, ConnectedRouter} from 'connected-react-router'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {Provider} from 'react-redux'
 
 import createRootReducer from 'reducers'
-import Layout from 'containers/layout'
+import routes from 'routes'
 
 const history = createBrowserHistory()
-const middleweres = [thunk, routerMiddleware(history)]
+const middlewares = [thunk, routerMiddleware(history)]
 const store = createStore(
   createRootReducer(history),
-  composeWithDevTools(applyMiddleware(...middleweres))
+  composeWithDevTools(applyMiddleware(...middlewares))
 )
-
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Layout />
+      {routes}
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
